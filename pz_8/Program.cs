fc9702958771c4b4144aae06c73cff460ff56601
19032004katya@gmail.com <19032004katya@gmail.com>
2021-10-15 16:56:40 +0300

    pz8

--- pz_8/Program.cs
+++ pz_8/Program.cs
@@ -10,55 +10,62 @@ namespace pz_8
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
-            int lastZeroQuantity = 0;
-            int[] A = new int[50];
+            Console.WriteLine("работа с git");
 
-            for (int i = 0; i < A.Length; i++)
+
+
+
             {
-                A[i] = rand.Next(20, 100);
-                if (A[i] % 10 == 0)
-                {
-                    lastZeroQuantity++;
-                }
-            }
+                Random rand = new Random();
+                int lastZeroQuantity = 0;
+                int[] A = new int[50];
 
-            foreach (int n in A)
-            {
-                Console.WriteLine(n);
-            }
-            Console.WriteLine();
-
-
-
-
-            if (lastZeroQuantity != 0)
-            {
-                Console.WriteLine("Числа оканчивающиеся на 0");
-
-                int[] B = new int[lastZeroQuantity];
-
-                int j = 0;
                 for (int i = 0; i < A.Length; i++)
                 {
+                    A[i] = rand.Next(20, 100);
                     if (A[i] % 10 == 0)
                     {
-                        B[j] = A[i];
-                        j++;
+                        lastZeroQuantity++;
                     }
                 }
 
-                foreach (int n in B)
+                foreach (int n in A)
                 {
                     Console.WriteLine(n);
                 }
-            }
-            else
-            {
-                Console.WriteLine("Чисел оканчивающийся на 0 нет");
-            }
+                Console.WriteLine();
 
 
-        }
-    }
+
+
+                if (lastZeroQuantity != 0)
+                {
+                    Console.WriteLine("Числа оканчивающиеся на 0");
+
+                    int[] B = new int[lastZeroQuantity];
+
+                    int j = 0;
+                    for (int i = 0; i < A.Length; i++)
+                    {
+                        if (A[i] % 10 == 0)
+                        {
+                            B[j] = A[i];
+                            j++;
+                        }
+                    }
+
+                    foreach (int n in B)
+                    {
+                        Console.WriteLine(n);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Чисел оканчивающийся на 0 нет");
+                }
+
+
+            }
+
+        } }
 }
